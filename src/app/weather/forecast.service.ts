64eef69a4a596426fefb9bc2c864f936abb680f4
Azdevpp <Azdevpp@gmail.com>
2022-12-25 00:49:11 +0200

    implementing the function getForecast in the forecastService with observebles

--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ForecastService {
   constructor() {}
+
+  getForecast() {
+    return this.getCurrentLocation().pipe(
+      map((coords) => {
+        return new HttpParams()
+          .set('lat', String(coords.latitude))
+          .set('lon', String(coords.longitude))
+          .set('units', 'metric')
+          .set('appid', '404f9fdbaf8723e7fe09ce59ad026fc3');
+      })
+    );
+  }
 
   getCurrentLocation() {
     return new Observable<GeolocationCoordinates>((observer) => {
