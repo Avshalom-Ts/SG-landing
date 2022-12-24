a954d5bae4deeada973ba0bb51bb339a2f0a2c99
Azdevpp <Azdevpp@gmail.com>
2022-12-25 00:36:59 +0200

    Get geolocation function with observable from user browser

--- src/app/weather/forecast/forecast.component.ts
+++ src/app/weather/forecast/forecast.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { ForecastService } from '../forecast.service';
 
 @Component({
   selector: 'app-forecast',
   templateUrl: './forecast.component.html',
-  styleUrls: ['./forecast.component.css']
+  styleUrls: ['./forecast.component.css'],
 })
 export class ForecastComponent {
-
+  constructor(forecastService: ForecastService) {
+    forecastService.getCurrentLocation().subscribe((coords) => {
+      console.log(coords);
+    });
+  }
 }
